8654a93d91c41378db254632ca77dfad13bc766a
CaptainChala <cchala@utem.cl>
2025-05-08 10:40:04 -0400

    Creación de archivos PacienteControlador,Paciente,PacienteRepositorio,paciente_form,paciente_admin y Modifición menor en index.html

---
+++ src/main/java/com/obstetricia/segurity/obstetricia/modelo/Paciente.java
@@ -0,0 +1,111 @@
+package com.obstetricia.segurity.obstetricia.modelo;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Paciente {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String nombreCompleto;
+    private LocalDate fechaNacimiento;
+    private String sexo;
+    private String region;
+    private String ciudad;
+    private String direccion;
+    private String rut;
+    private String telefono;
+    private String correo;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+        public int getEdad() {
+        if (fechaNacimiento == null) {
+            return 0; // o lanza una excepción o devuelve -1 según tu lógica
+        }
+        return Period.between(fechaNacimiento, LocalDate.now()).getYears();
+    }
+}
