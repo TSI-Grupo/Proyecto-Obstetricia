b2d8676af69afda3ecc5f6395d92edd73849bd2e
Benjvdiaz <benja.diaz.gonzalez@gmail.com>
2025-06-16 21:30:49 -0400

    Correción de bug en eliminación de usuario via panel de admin

--- src/main/java/com/obstetricia/segurity/obstetricia/modelo/Usuario.java
+++ src/main/java/com/obstetricia/segurity/obstetricia/modelo/Usuario.java
@@ -34,7 +34,7 @@ public class Usuario {
 
 
 
-    @ManyToMany(fetch = FetchType.EAGER,cascade = CascadeType.ALL)
+    @ManyToMany(fetch = FetchType.EAGER, cascade = { CascadeType.PERSIST, CascadeType.MERGE })
     @JoinTable(
         name = "usuarios_roles",
         joinColumns = @JoinColumn(name = "usuario_id",referencedColumnName = "id"),
